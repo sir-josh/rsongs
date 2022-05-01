cb67af3dd047af95ed04ec7f4a68a96ea115e357
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-05-01 19:18:09 +0100

    code: add reducers and combine them using combineReducer func from redux lib

---
+++ src/reducers/index.js
@@ -0,0 +1,25 @@
+import { combineReducer } from 'redux';
+
+const songReducer = () => {
+    return [
+        { title: 'No Scrubs', duration: '2:55'},
+        { title: 'Tie Me Down(with Elley Duhe', duration: '3:11'},
+        { title: 'Better', duration: '2:12'},
+        { title: 'Happy Now', duration: '3:52'},
+        { title: 'Don\'t Leave Me Alone', duration: '4:09'},
+        { title: 'Girls Like You', duration: '3:55'}
+    ];
+}
+
+const selectedSongReducer = (selectedSong = null, action ) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+
+    return selectedSong;
+}
+
+export default combineReducer({
+    songs: songReducer,
+    selectedSong: selectedSongReducer
+});
