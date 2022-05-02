83bcb9c11e2fa1d8425d4b64be1530f94c98c718
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-05-02 12:20:46 +0100

    code: add functional component with connect

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectedSongs } from '../actions';
 
 class Songlist extends Component {
     renderList(){
         return this.props.songs.map((song, index) => (
             <div className='item' key={index}>
                 <div className='right floated content'>
-                    <button className='ui button primary'>Select</button>
+                    <button 
+                        className='ui button primary'
+                        onClick={() => this.props.selectedSongs(song)}
+                    >
+                        Select
+                    </button>
                 </div>
                 <div className='content'>
                     {song.title}
@@ -23,7 +29,8 @@ class Songlist extends Component {
 }
 
 const mapStateToProps = state => {
+    console.log(state);
     return { songs: state.songs}
 }
 
-export default connect(mapStateToProps)(Songlist);
+export default connect(mapStateToProps, { selectedSongs })(Songlist);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux';
 const songReducer = () => {
     return [
         { title: 'No Scrubs', duration: '2:55'},
-        { title: 'Tie Me Down(with Elley Duhe', duration: '3:11'},
+        { title: 'Tie Me Down(with Elley Duhe)', duration: '3:11'},
         { title: 'Better', duration: '2:12'},
         { title: 'Happy Now', duration: '3:52'},
         { title: 'Don\'t Leave Me Alone', duration: '4:09'},
